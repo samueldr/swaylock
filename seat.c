6b30e6376a6ab96cc7449c7fd3834a5411bc13d3
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-11-21 01:48:16 -0500

    WIP: Add boilerplate for touch handling

--- seat.c
+++ seat.c
@@ -183,6 +183,46 @@ static const struct wl_pointer_listener pointer_listener = {
 	.axis_discrete = wl_pointer_axis_discrete,
 };
 
+static void wl_touch_down(void *data, struct wl_touch *wl_touch, uint32_t serial, uint32_t time, struct wl_surface *surface, int id, wl_fixed_t x, wl_fixed_t y) {
+	if (id == 0) {
+		state->touched_surface = surface;
+
+		wl_list_for_each(swaylock_surface, &state->surfaces, link) {
+			if (surface == swaylock_surface->child) {
+				break;
+			}
+		}
+	}
+}
+
+static void wl_touch_up(void *data, struct wl_touch *wl_touch, uint32_t serial, uint32_t time, int id) {
+	if (id == 0) {
+	}
+}
+
+static void wl_touch_motion(void *data, struct wl_touch *wl_touch, uint32_t time, int id, wl_fixed_t x, wl_fixed_t y) {
+	if (id == 0) {
+		int scaled_x = wl_fixed_to_double(x);
+		int scaled_y = wl_fixed_to_double(y);
+	}
+}
+
+static void wl_touch_frame(void *data, struct wl_touch *wl_touch) {
+	// Who cares
+}
+
+static void wl_touch_cancel(void *data, struct wl_touch *wl_touch) {
+	// Who cares
+}
+
+static const struct wl_touch_listener touch_listener = {
+	.down = wl_touch_down,
+	.up = wl_touch_up,
+	.motion = wl_touch_motion,
+	.frame = wl_touch_frame,
+	.cancel = wl_touch_cancel,
+};
+
 static void seat_handle_capabilities(void *data, struct wl_seat *wl_seat,
 		enum wl_seat_capability caps) {
 	struct swaylock_seat *seat = data;
@@ -194,6 +234,10 @@ static void seat_handle_capabilities(void *data, struct wl_seat *wl_seat,
 		wl_keyboard_release(seat->keyboard);
 		seat->keyboard = NULL;
 	}
+	if (seat->touch) {
+		wl_touch_release(seat->touch);
+		seat->touch = NULL;
+	}
 	if ((caps & WL_SEAT_CAPABILITY_POINTER)) {
 		seat->pointer = wl_seat_get_pointer(wl_seat);
 		wl_pointer_add_listener(seat->pointer, &pointer_listener, NULL);
@@ -201,6 +245,10 @@ static void seat_handle_capabilities(void *data, struct wl_seat *wl_seat,
 	if ((caps & WL_SEAT_CAPABILITY_KEYBOARD)) {
 		seat->keyboard = wl_seat_get_keyboard(wl_seat);
 		wl_keyboard_add_listener(seat->keyboard, &keyboard_listener, seat);
+	}
+	if ((caps & WL_SEAT_CAPABILITY_TOUCH)) {
+		seat->touch = wl_seat_get_touch(wl_seat);
+		wl_touch_add_listener(seat->touch, &touch_listener, seat);
 	}
 }
 
